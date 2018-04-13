1c24672f3909d8d8364c02181421ef532f5e57cf
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-04-12 22:19:54 -0500

    added code to save partial blocks

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get("/articles/:id", function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/notes/:id", function(req, res) {
   
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
